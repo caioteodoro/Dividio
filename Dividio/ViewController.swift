c40a088af6f56ba69e432bdf1baa44fa6af11231
caioteodoro <caiorodrigot@hotmail.com>
2022-01-19 18:18:59 -0300

    Initial Commit

---
+++ Dividio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dividio
+//
+//  Created by Caio Teodoro on 19/01/22.
+//  Copyright © 2022 Caio Teodoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
