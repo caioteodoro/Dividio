a02e0ef5aa44663283ab3b448ac33c10ae235e99
caioteodoro <caiorodrigot@hotmail.com>
2022-01-23 02:59:54 -0300

    second screen almost done

---
+++ Dividio/PaymentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PaymentsViewController.swift
+//  Dividio
+//
+//  Created by Caio Teodoro on 23/01/22.
+//  Copyright © 2022 Caio Teodoro. All rights reserved.
+//
+
+import UIKit
+
+class PaymentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
