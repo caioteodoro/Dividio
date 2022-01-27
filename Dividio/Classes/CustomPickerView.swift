987314688274a4073a70d603a8700e297dd7c3ed
caioteodoro <caiorodrigot@hotmail.com>
2022-01-26 23:42:01 -0300

    setting functions to add payment rows to tableview

--- Dividio/Classes/CustomPickerView.swift
+++ Dividio/Classes/CustomPickerView.swift
@@ -13,13 +13,17 @@ import UIKit
     
     let rotationAngle: CGFloat! = -90  * (.pi/180)
     
+    
     func setup() {
         self.layer.borderColor = #colorLiteral(red: 0.2705882353, green: 0.2705882353, blue: 0.2705882353, alpha: 1);
         self.layer.borderWidth = 3.0;
-        self.transform = CGAffineTransform(rotationAngle: rotationAngle);
-        self.frame = CGRect(x: -150, y: 100.0, width: self.bounds.width + 300, height: 200)
-    }
+        self.layer.shadowColor = #colorLiteral(red: 0.4705882353, green: 0.3529411765, blue: 0.8078431373, alpha: 1)
+        self.layer.shadowOffset = CGSize(width: -4, height: 4);
+        //self.transform = CGAffineTransform(rotationAngle: rotationAngle);
+        //self.frame = CGRect(x: -150, y: 100.0, width: self.bounds.width + 300, height: 200)
         
+    }
+         
         
     override init(frame: CGRect) {
         super.init(frame: frame);
--- Dividio/PaymentsViewController.swift
+++ Dividio/PaymentsViewController.swift
@@ -13,7 +13,34 @@ class PaymentsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     var people: [Person] = [];
     var items: [Item] = [];
     
+    @IBOutlet weak var paymentTextField: WhiteTextField!
     @IBOutlet weak var personPicker: CustomPickerView!
+    @IBOutlet weak var paymentsTableView: TableLayout!
+    @IBOutlet weak var continueButton: PurpleButton!
+    
+    @IBAction func addPaymentButton(_ sender: Any) {
+        let newPersonName = people[selected].name
+        let newPayment = Double(paymentTextField.text!)
+        if !newPersonName.isEmpty && newPayment != nil {
+            people[selected].payment = newPayment!
+            paymentsTableView.beginUpdates();
+            paymentsTableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .right);
+            paymentsTableView.endUpdates();
+            //nameTextField.text = ""; tem como mover o seletor de nomes automaticamente?
+            paymentTextField.text = "";
+            allowContinueButton ()
+        }
+    }
+    
+    
+    func allowContinueButton () {
+        var paymentsAmount: Double = 0;
+        for i in 0..<(people.count-1) {
+            paymentsAmount += people[i].payment;
+        }
+        if paymentsAmount == 0 { continueButton.isEnabled = false;
+        } else { continueButton.isEnabled = true; }
+    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -21,6 +48,12 @@ class PaymentsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return people.count
+    }
+    
+    var selected = 0;
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selected = row
     }
     
     //func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -43,9 +76,19 @@ class PaymentsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
       return 100
     }
     
+    func paymentsPickerViewSettings () {
+        let rotationAngle: CGFloat! = -90  * (.pi/180)
+            let y = personPicker.frame.origin.y;
+            personPicker.layer.shadowColor = #colorLiteral(red: 0.4705882353, green: 0.3529411765, blue: 0.8078431373, alpha: 1)
+            personPicker.layer.shadowOffset = CGSize(width: -4, height: 4);
+            personPicker.transform = CGAffineTransform(rotationAngle: rotationAngle);
+            personPicker.frame = CGRect(x: paymentTextField.frame.origin.x, y: y - 15, width: view.frame.width - 94, height: 45)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.personPicker.delegate = self;
         self.personPicker.dataSource = self;
+        paymentsPickerViewSettings();
     }
 }
